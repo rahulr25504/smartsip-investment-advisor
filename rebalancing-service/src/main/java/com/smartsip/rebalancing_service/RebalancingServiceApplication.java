6dabaa761d05a4bb42152c554109c72aae119074
rahulr25504 <rahulr25504@gmail.com>
2025-11-07 13:26:13 +0530

    Add all microservices with Spring Initializr

---
+++ rebalancing-service/src/main/java/com/smartsip/rebalancing_service/RebalancingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smartsip.rebalancing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebalancingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebalancingServiceApplication.class, args);
+	}
+
+}
---
+++ rebalancing-service/src/test/java/com/smartsip/rebalancing_service/RebalancingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartsip.rebalancing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebalancingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
